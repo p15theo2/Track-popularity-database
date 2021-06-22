86f282bf6d27c18647f96985eec3b05c3d02c636
Spiros_t2 <spirostheo2@gmail.com>
2021-06-23 00:57:02 +0300

    Update 1. First csv maker .py

--- code examples/1. First csv maker .py
+++ code examples/1. First csv maker .py
@@ -29,14 +29,6 @@ def returnque(querry):
 results = returnque("""SELECT track.title,artist.name,track.id_video FROM track
                         LEFT JOIN  artist_track ON track.id = artist_track.track_id 
                          LEFT JOIN artist ON artist_track.artist_id = artist.id""")
-results = returnque("""SELECT tr.title ,date_format(spo.date,"%Y-%c-%d"),spo.position FROM spotify_rating spo 
-                       left JOIN track tr 
-                       on spo.track_id = tr.id;""")
-results = returnque("""SELECT tr.id_video,tr.title,ar.name,date_format(spo.date,"%Y-%c-%d"),spo.position
-                       from spotify_rating spo
-                       left join track tr on spo.track_id = tr.id
-                       left join artist_track on tr.id = artist_track.track_id 
-                       left join artist ar on artist_track.artist_id = ar.id;""")
 with open('alltracks.csv', 'w', newline='') as f_handle:
     writer = csv.writer(f_handle)
     # Add the header/column names
